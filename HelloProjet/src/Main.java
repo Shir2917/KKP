812d7cf2a359fdf9c78416ac7c0df5349ce19574
Shir <985303194@qq.com>
2019-06-11 21:16:42 +0800

    本次修改将添加到本地并上传github

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,4 +6,9 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
     }
+
+    @Test
+    public void show01(){
+        System.out.println("HelloGit");
+    }
 }
